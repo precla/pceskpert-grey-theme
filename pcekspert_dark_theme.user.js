500adb5d867acffcf291a08fb0a716205f7ef434
Antonio Prcela <antonio.prcela@gmail.com>
2020-05-21 13:06:18 +0200

    bump version

--- pcekspert_dark_theme.user.js
+++ pcekspert_dark_theme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PCEkspert Dark Theme
-// @version      0.2.0
+// @version      0.2.1
 // @namespace    https://github.com/precla/pceskpert-themes/pcekspert_dark_theme
 // @description  very dark forum theme
 // @author       precla / pci_e3x
